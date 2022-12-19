ac656cd0f6a4ab5afd8f4a3d0019d9120ee82714
olkoa003 <43721193+olkoa003@users.noreply.github.com>
2022-12-19 11:58:35 +0100

    has tested the API using JEST

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-// const main = async () => {
-
-//     // const user = await User.findById('639cd8747f531b487c5e1ca6')
-//     // await user.populate('tasks').execPopulate()
-//     // console.log(user.tasks)
-// }
-
-// main()
